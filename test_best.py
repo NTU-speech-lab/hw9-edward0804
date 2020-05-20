dc6719a50391c5f0fef8e809f13d5b8999840142
Edward <edward@Edwardde-MacBook-Pro.local>
2020-05-21 03:20:22 +0800

    first commit

---
+++ test_best.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from sklearn.decomposition import KernelPCA
+from sklearn.manifold import TSNE
+from sklearn.cluster import MiniBatchKMeans
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import sys
+
+
+def preprocess(image_list):
+    image_list = np.array(image_list)
+    image_list = np.transpose(image_list, (0, 3, 1, 2))
+    image_list = (image_list / 255.0) * 2 - 1
+    image_list = image_list.astype(np.float32)
+    return image_list
+class Image_Dataset(Dataset):
+    def __init__(self, image_list):
+        self.image_list = image_list
+    def __len__(self):
+        return len(self.image_list)
+    def __getitem__(self, idx):
+        images = self.image_list[idx]
+        return images
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, 5, stride=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 9, stride=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 17, stride=1),
+            nn.Tanh()
+        )
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x  = self.decoder(x1)
+        return x1, x
+
+def cal_acc(gt, pred):
+    correct = np.sum(gt == pred)
+    acc = correct / gt.shape[0]
+    return max(acc, 1-acc)
+
+def inference(X, model, batch_size=256):
+    X = preprocess(X)
+    dataset = Image_Dataset(X)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    latents = []
+    for i, x in enumerate(dataloader):
+        x = torch.FloatTensor(x)
+        vec, img = model(x.cuda())
+        if i == 0:
+            latents = vec.view(img.size()[0], -1).cpu().detach().numpy()
+        else:
+            latents = np.concatenate((latents, vec.view(img.size()[0], -1).cpu().detach().numpy()), axis = 0)
+    print('Latents Shape:', latents.shape)
+    return latents
+
+def predict(latents):
+    random_seed = 20
+    # First Dimension Reduction
+    transformer = KernelPCA(n_components=200, kernel='rbf', n_jobs=-1 , random_state=random_seed)
+    kpca = transformer.fit_transform(latents)
+    print('First Reduction Shape:', kpca.shape)
+
+    # # Second Dimesnion Reduction
+    X_embedded = TSNE(n_components=2 ,  random_state=random_seed).fit_transform(kpca)
+    print('Second Reduction Shape:', X_embedded.shape)
+
+    # Clustering
+    pred = MiniBatchKMeans(n_clusters=2, random_state=0).fit(X_embedded)
+    pred = [int(i) for i in pred.labels_]
+    pred = np.array(pred)
+    return pred, X_embedded
+
+def invert(pred):
+    return np.abs(1-pred)
+
+def save_prediction(pred, out_csv='prediction.csv'):
+    with open(out_csv, 'w') as f:
+        f.write('id,label\n')
+        for i, p in enumerate(pred):
+            f.write(f'{i},{p}\n')
+    print(f'Save prediction to {out_csv}.')
+
+trainX = np.load(sys.argv[1])
+# load model
+model = AE().cuda()
+
+model.load_state_dict(torch.load(sys.argv[2]))
+model.eval()
+valX = np.load('valX.npy')
+valY = np.load('valY.npy')
+latents = inference(valX, model)
+pred_from_latent, emb_from_latent = predict(latents)
+acc_latent = cal_acc(valY, pred_from_latent)
+print('The clustering accuracy is:', acc_latent)
+
+latents = inference(X=trainX, model=model)
+pred, X_embedded = predict(latents)
+
+save_prediction(invert(pred), sys.argv[3])
+
